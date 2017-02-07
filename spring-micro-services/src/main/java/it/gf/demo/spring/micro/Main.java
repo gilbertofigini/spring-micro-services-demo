aa22243382127b5f85e574144c63221a3e1f5d64
Gilberto Figini <gilberto.figini@gmail.com>
2017-02-07 23:26:22 +0100

    created Main class

---
+++ spring-micro-services/src/main/java/it/gf/demo/spring/micro/Main.java
@@ -0,0 +1,20 @@
+package it.gf.demo.spring.micro;
+
+public class Main {
+	public static void main(String[] args) {
+//		String serverName = "NO-VALUE";
+		
+		switch (args.length) {
+			default:
+				usage();
+				return;
+		}
+	}
+	
+	protected static void usage() {
+		System.out.println("Usage: java -jar ... <server-name> [server-port]");
+		System.out.println(
+				"     where server-name is 'reg', 'registration', " + "'accounts' or 'web' and server-port > 1024");
+	}
+
+}
